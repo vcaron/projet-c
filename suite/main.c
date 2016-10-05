97e3c2ef4497dbf1821a95d5b34118d34c54da76
vcaron <caron.vincent@outlook.fr>
2016-10-05 14:56:58 +0200

    05/10/2016

---
+++ suite/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+
+while(1)
+{
+ int resultat = 0, nombre1 = 0, nombre2 = 0;
+
+    // demande de deux nombres :
+
+    printf("saisir un nombre : \n");
+    scanf("%d", &nombre1);
+    printf(" saisir deuxieme nombre : \n");
+    scanf("%d", &nombre2);
+
+    // on fait le calcul
+
+    resultat = nombre1 + nombre2;
+
+    // afficher texte en fonction de la réponse
+if (resultat < 20)
+printf("KO inferieur ou egale a 20");
+else if (resultat >= 20)
+printf("OK superieur ou egale a 20");
+printf("\n\n");
+
+
+
+}
+}
---
+++ triangle (ex33 niveau 2).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+  int x, y, z,d;
+  // demande du nombre de ligne
+
+printf ("nombre de ligne ? ");
+scanf ("%d",&d);
+
+
+for(x=0;x<d;x++)
+{
+for(y=0;y<d;y++)
+{
+printf(" ");
+}
+
+
+for(z=0;z<=x;z++)
+{
+printf("*");
+}
+printf("\n");
+
+}
+
+}
+
+
